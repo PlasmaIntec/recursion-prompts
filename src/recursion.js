7db7cc24fbffb6ddf11d5d906024fbcddf2ba348
Matthew Ma <alphaintec@gmail.com>
2018-12-28 22:04:13 -0800

    completed some challenges

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,50 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if (n < 0) return null;
+	if (n === 1 || n === 0) return 1;
+	return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (array.length === 0) return 0;
+	if (array.length > 1) return array[array.length-1] + sum(array.slice(0,-1));
+	return array[0];
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	if (array.length === 0) return 0;
+	if (Array.isArray(array[0])) return arraySum(array[0]) + arraySum(array.slice(1));
+	return array[0] + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	if (n === 0) return true;
+	if (n === 1) return false;
+	if (n > 0) return isEven(n-2);
+	return isEven(n+2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if (n === 0) return 0;
+	if (n < 0) return n+1 + sumBelow(n+1);
+	return n-1 + sumBelow(n-1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+	if (y - x > 1) return [x+1].concat(range(x+1,y));
+	if (x - y > 1) return [x-1].concat(range(x-1,y));
+	return [];
 };
 
 // 7. Compute the exponent of a number.
@@ -40,6 +59,9 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+	if (exp === 0) return 1;
+	if (exp < 0) return exponent(base, exp+1) / base; // NOTE ORDER MATTERS- 1/base * exponent(base, exp+1) will give truncating errors
+	return base * exponent(base, exp-1);
 };
 
 // 8. Determine if a number is a power of two.
@@ -47,14 +69,25 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+	if (n === 1) return true;
+	if (n < 1) return false;
+	return powerOfTwo(n/2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+	if (string.length > 0) return string[string.length-1] + reverse(string.slice(0,-1));
+	return '';
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	string = string.replace(/\s/g, '').toLowerCase();
+	if (string.length > 0) {
+		if (string[0] === string[string.length-1]) return palindrome(string.slice(1,-1));
+		return false;
+	}
+	return true;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -63,16 +96,50 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+	if (y === 0) return NaN;
+	if (x > 0) {
+		if (x >= y) return modulo(x-y, y);
+		return x;	
+	} else {
+		if (y > 0) {
+			if (x <= -y) return modulo(x+y, y);
+			return x;
+		} else {
+			if (x <= y) return modulo(x-y, y);
+			return x;
+		}
+	}
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+	if (y === 0) return 0;
+	if (y > 0) return x + multiply(x, y-1);
+	return multiply(x, y+1) - x;
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+	if (y === 0) return NaN;
+	if (y > 0) {
+		if (x > 0) {
+			if (x >= y) return 1 + divide(x-y, y); 
+			return 0;
+		} else {
+			if (-x >= y) return -1 + divide(x+y, y); 
+			return 0;
+		}
+	} else {
+		if (x > 0) {
+			if (x >= -y) return -1 + divide(x-y, y); 
+			return 0;
+		} else {
+			if (-x >= -y) return 1 + divide(x+y, y); 
+			return 0;
+		}
+	}
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
